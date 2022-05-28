82e07b3da74540a5e81d45c87d708168f8cab408
Jarod Florence <jarod.florence@outlook.com>
2022-05-28 10:43:03 -0500

    AboutMe initial content added

---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function AboutMe() {
+    return (
+       <section>
+       <div>
+        <h1>About Me</h1>
+        <p>I am Jarod. A FullStack MERN web developer. I am nearing the end of the UT Austin Online Coding Boot Camp.
+            During my time in the program I have developed skills in HTML, CSS, JS, SQL, NoSQL and MERN stack as well 
+            as other helpful tools such as JQuery, Bootstrap, Insomnia and GraphQL.
+            
+            My goal is to continue building on the foundations set in the program to fulfill a career in web and app 
+            development.</p>
+        </div>
+        <div>
+            {/* add an image here */}
+        </div>
+        </section>
+    );
+}
